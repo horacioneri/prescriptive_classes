855837050dc764e37bec113333467f896d80f3e6
horacioneri <horacio.neri@ltplabs.com>
2024-11-21 08:41:08 +0000

    Add summarization

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,10 +71,10 @@ elif 0 < current_page <= len(page_title)-1:
 
     # Summarize the solution found
     area_used = sum(answers)
-    st.text_area(label="Area used:", value=area_used, height=30)
+    st.text_area(label="Area used:", value=area_used, height=68)
 
     sales_total = sales_total = sum([a * e for a, e in zip(answers, linear_space_elasticities)])
-    st.text_area(label="Total expected sales:", value=sales_total, height=30)
+    st.text_area(label="Total expected sales:", value=sales_total, height=68)
     
 # Display buttons at the end to navigate between pages
 if current_page == 0:
